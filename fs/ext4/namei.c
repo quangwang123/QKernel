d84d7659ef04870d3ad33a4d2f098fe4fc944d9c
LuanHalaiko <luhalaiko@gmail.com>
2022-01-25 12:50:17 -0300

    fs/include: fix unicode implementation

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1311,7 +1311,7 @@ void ext4_fname_setup_ci_filename(struct inode *dir, const struct qstr *iname,
 {
 	int len;
 
-	if (!needs_casefold(dir)) {
+	if (!IS_CASEFOLDED(dir)) {
 		cf_name->name = NULL;
 		return;
 	}
@@ -1357,7 +1357,7 @@ static inline bool ext4_match(const struct inode *parent,
 #endif
 
 #ifdef CONFIG_UNICODE
-	if (needs_casefold(parent)) {
+	if (IS_CASEFOLDED(parent)) {
 		if (fname->cf_name.name) {
 			struct qstr cf = {.name = fname->cf_name.name,
 					  .len = fname->cf_name.len};
