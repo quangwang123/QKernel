cfbbea41660f8338e4a6176e415e59c339fe0908
fukiame <fukiame@proton.me>
2024-12-29 22:11:51 +0700

    [SQUASH] treewide: drop mtk sched_mon
    Revert "[ALPS05068169] timer: add debug dump for wdt timeout scenario, commit 7ab4757cd8c79c78ad80fe7d2aa10e397afd5f96.
    Revert "[ALPS05216550] mtprof: disable death signal log, commit 1079002d8b892ccb7f834ce592d0dc15398a1605.
    Revert "[ALPS05204026] mtprof: fix converity scan, commit 11109fc74ac20884ad9447dffd5c36454836832c.
    Revert "[ALPS05125330] mtprof: fix converity, commit 80d27aba419272f4dfd2babc670b97837051c0c5.
    Revert "[ALPS05167835] shed_mon: add usb0 period setting, commit c20743422582761c274c80c3a13998895d6562f8.
    Revert "[ALPS05165852] sched_mon: change threshold of debug log, commit 56b468e11043c1f7bbe9951af5d9f69bc43b6087.
    Revert "[ALPS05149462] sched_mon: add period setting for usb0, commit 3cb188835dae1f9a9bc5efc20131cf6357a2f0c1.
    Revert "[ALPS04997450] sched_mon: fix coding defect, commit a200bca9e2b68abafc84dc7e93f7b08127095a42.
    Revert "[ALPS04993096] sched_mon: improve irq count debug feature, commit 401df9fc50919f80732a2862f2bb97c71f2cf737.
    Revert "[ALPS04993096] sched_mon: modify macro definition, commit dd487c5cc1413e0aed207433b5d682bfc4cbd3c0.
    Revert "[ALPS04851942] sched_mon: use correct aee header file, commit 386924f0da06c2d28a86570d1e3b8ca110d1bde8.
    Revert "[ALPS05095852] lockdep: fix warning of irq off tracer, commit 5e87ebd85e95c8613072219b34accbc3af1ed58a.
    Revert "[ALPS04851942] sched_mon: implement schedule monitor v2, commit ccad4e94282681b3e640e072f3c99a36b2017cd7.
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1318,7 +1318,6 @@ EXPORT_SYMBOL(del_timer_sync);
 static void call_timer_fn(struct timer_list *timer, void (*fn)(struct timer_list *))
 {
 	int count = preempt_count();
-	unsigned long long ts;
 
 #ifdef CONFIG_LOCKDEP
 	/*
@@ -1340,9 +1339,7 @@ static void call_timer_fn(struct timer_list *timer, void (*fn)(struct timer_list
 	lock_map_acquire(&lockdep_map);
 
 	trace_timer_expire_entry(timer);
-	check_start_time(ts);
 	fn(timer);
-	check_process_time("timer %ps", ts, fn);
 	trace_timer_expire_exit(timer);
 
 	lock_map_release(&lockdep_map);
