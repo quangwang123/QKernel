4aadb6583314dafbb2132d82c51bae244c676d32
quangwang123 <quang@Admin.localdomain>
2025-08-17 15:58:01 +0700

    OV

--- drivers/cpufreq/mediatek-cpufreq.c
+++ drivers/cpufreq/mediatek-cpufreq.c
@@ -25,10 +25,10 @@
 #include <linux/slab.h>
 #include <linux/thermal.h>
 
-#define MIN_VOLT_SHIFT		(10000)
-#define MAX_VOLT_SHIFT		(20000)
-#define MAX_VOLT_LIMIT		(145000)
-#define VOLT_TOL		(1000)
+#define MIN_VOLT_SHIFT		(100000)
+#define MAX_VOLT_SHIFT		(200000)
+#define MAX_VOLT_LIMIT		(1450000)
+#define VOLT_TOL		(10000)
 
 /*
  * The struct mtk_cpu_dvfs_info holds necessary information for doing CPU DVFS
