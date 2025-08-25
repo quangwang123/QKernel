c0abc5a6686b70df76d2d204c4cb70fa2266e291
quangwang123 <quang@Admin.localdomain>
2025-08-25 19:59:53 +0700

    disable eem

--- drivers/misc/mediatek/base/power/upower_v2/mtk_unified_power.c
+++ drivers/misc/mediatek/base/power/upower_v2/mtk_unified_power.c
@@ -334,7 +334,7 @@ static void upower_init_rownum(void)
 static unsigned int eem_is_enabled(void)
 {
 /* #ifndef EARLY_PORTING_EEM */
-	return mt_eem_is_enabled();
+	return 0;
 }
 
 static void upower_wait_for_eem_volt_done(void)
