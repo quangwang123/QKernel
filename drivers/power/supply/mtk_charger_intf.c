606993b3fc49a08bb8bd0c22fa315babf6283cb5
quangwang123 <superidolniseolung@gmail.com>
2025-10-07 19:59:10 +0700

    Revert "[ALPS05818407] mt6761:Plugin Non-standard TA detected as PC usb port(SDP)"
    This reverts commit aec31ab6015f70d378cc0e01833f477ff2e85030.

--- drivers/power/supply/mtk_charger_intf.c
+++ drivers/power/supply/mtk_charger_intf.c
@@ -313,9 +313,6 @@ int get_charger_type(struct mtk_charger *info)
 		if (prop.intval == 0)
 			prop2.intval = POWER_SUPPLY_TYPE_UNKNOWN;
 		else if (prop2.intval == POWER_SUPPLY_TYPE_USB &&
-		    prop3.intval == POWER_SUPPLY_USB_TYPE_UNKNOWN)
-			prop2.intval = POWER_SUPPLY_TYPE_UNKNOWN;
-		else if (prop2.intval == POWER_SUPPLY_TYPE_USB &&
 		    prop3.intval == POWER_SUPPLY_USB_TYPE_DCP)
 			prop2.intval = POWER_SUPPLY_TYPE_USB_FLOAT;
 	}
