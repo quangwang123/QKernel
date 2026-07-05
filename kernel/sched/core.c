4b80c329e9eb08e35ae6abd907648c6bf3d6bdc1
danya2271 <danya2271@yandex.ru>
2026-07-05 16:33:38 +0500

    sched: drop mtk perf tick hook

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -18,9 +18,6 @@
 #include "../workqueue_internal.h"
 #include "../smpboot.h"
 
-#ifdef CONFIG_MTK_PERF_COMMON
-#include <mt-plat/perf_common.h>
-#endif
 #include "pelt.h"
 
 #define CREATE_TRACE_POINTS
@@ -3999,9 +3996,6 @@ void scheduler_tick(void)
 	rq_unlock(rq, &rf);
 
 	perf_event_task_tick();
-#ifdef CONFIG_MTK_PERF_COMMON
-	perf_common(ktime_get_ns());
-#endif
 #ifdef CONFIG_SMP
 	rq->idle_balance = idle_cpu(cpu);
 	trigger_load_balance(rq);
