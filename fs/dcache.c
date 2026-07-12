c01169f2eb24f1536da234806f0c980fa78001b8
danya2271 <danya2271@yandex.ru>
2026-07-13 01:01:53 +0500

    fs: dcache: right-size main hash for low-RAM builds

--- fs/dcache.c
+++ fs/dcache.c
@@ -3078,6 +3078,10 @@ static void __init dcache_init_early(void)
 	if (hashdist)
 		return;
 
+#ifdef CONFIG_MTK_ENABLE_GMO
+	if (!dhash_entries)
+		dhash_entries = 64 * 1024;
+#endif
 	dentry_hashtable =
 		alloc_large_system_hash("Dentry cache",
 					sizeof(struct hlist_bl_head),
@@ -3106,6 +3110,10 @@ static void __init dcache_init(void)
 	if (!hashdist)
 		return;
 
+#ifdef CONFIG_MTK_ENABLE_GMO
+	if (!dhash_entries)
+		dhash_entries = 64 * 1024;
+#endif
 	dentry_hashtable =
 		alloc_large_system_hash("Dentry cache",
 					sizeof(struct hlist_bl_head),
